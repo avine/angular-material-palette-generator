6fadbfc900d1bc5856c25ed0c4390b29a4c78e5c
Stéphane Francel <stephane.francel@zenika.com>
2025-04-07 01:50:09 +0200

    Minor change

--- palette-matching/index.ts
+++ palette-matching/index.ts
@@ -1,4 +1,5 @@
-// Run this script with Deno
+// Run this script with Deno and copy/paste the output in:
+//  `projects/angular-material-palette-generator/src/lib/palette-matching/palette-matching.ts`
 import { PALETTE_MATCHING_MAP } from './palette-matching.utils';
 
 console.log(PALETTE_MATCHING_MAP);
