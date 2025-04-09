3ef0ee254336cc4c1d26426e7bda6ef8a878eb3e
Stéphane Francel <stephane.francel@zenika.com>
2025-04-09 21:46:08 +0200

    Improve default footer in demo app

--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -1,11 +1,12 @@
 import { booleanAttribute, Component, input, ViewEncapsulation } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-layout',
   host: { class: 'app-layout' },
-  imports: [FooterComponent, HeaderComponent],
+  imports: [MatIconModule, FooterComponent, HeaderComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
   encapsulation: ViewEncapsulation.None,
