ec6eddcfe6d1d60e47497cb460a67b63a2aafcbb
Stéphane Francel <stephane.francel@zenika.com>
2025-04-22 23:41:02 +0200

    Switch to experimental Zoneless

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,8 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideTheme } from './shared/theme';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideTheme()],
+  providers: [provideExperimentalZonelessChangeDetection(), provideRouter(routes), provideTheme()],
 };
