03feda97999f8aac70962dfd9fffe9dad3845fab
Stéphane Francel <stephane.francel@zenika.com>
2025-04-25 12:10:41 +0200

    Improve default form value map

--- projects/angular-material-palette-generator/src/lib/palette-gen.config.ts
+++ projects/angular-material-palette-generator/src/lib/palette-gen.config.ts
@@ -14,10 +14,10 @@ export const MATERIAL_PALETTE_PERCENTAGES_MAP = {
 
 // Match `$azure-palette` from https://github.com/angular/components/blob/main/src/material/core/theming/_palettes.scss
 export const FORM_VALUE_MAP_DEFAULT: Record<PaletteName, PaletteGenFormValue> = {
-  primary: buildPaletteGenFormValue('#005cbb', { p1x: 0, p1y: 0.13, p2x: 0.9, p2y: 1 }),
-  secondary: buildPaletteGenFormValue('#565e71', { p1x: 0, p1y: 0.13, p2x: 0.91, p2y: 1 }),
-  tertiary: buildPaletteGenFormValue('#343dff', { p1x: 0, p1y: 0.15, p2x: 0.86, p2y: 1 }),
-  neutral: buildPaletteGenFormValue('#5e5e62', { p1x: 0, p1y: 0.11, p2x: 0.89, p2y: 1 }, true),
-  'neutral-variant': buildPaletteGenFormValue('#5b5e66', { p1x: 0, p1y: 0.11, p2x: 0.89, p2y: 1 }),
+  primary: buildPaletteGenFormValue('#005cbb', { p1x: 0, p1y: 0.21, p2x: 1, p2y: 1 }),
+  secondary: buildPaletteGenFormValue('#565e71', { p1x: 0, p1y: 0.2, p2x: 1, p2y: 1 }),
+  tertiary: buildPaletteGenFormValue('#343dff', { p1x: 0, p1y: 0.26, p2x: 1, p2y: 1 }),
+  neutral: buildPaletteGenFormValue('#5e5e62', { p1x: 0, p1y: 0.2, p2x: 1, p2y: 1 }, true),
+  'neutral-variant': buildPaletteGenFormValue('#5b5e66', { p1x: 0, p1y: 0.2, p2x: 1, p2y: 1 }),
   error: buildPaletteGenFormValue('#ba1a1a', { p1x: 0, p1y: 0.16, p2x: 0.88, p2y: 1 }),
 };
